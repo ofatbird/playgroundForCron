14462ae80a5a88a57e9f7dbfa82c7ccf68e5868b
Polly <973367375@qq.com>
2018-04-19 14:56:51 +0800

    u

--- mongodb/classify.js
+++ mongodb/classify.js
@@ -2,89 +2,100 @@ const jsonfile = require('jsonfile')
 
 const datamag = require('../json/magnet.json')
 
-const info = require('../json/wyinfo.json')
+const info = require('../json/wyfiltered.json')
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const Store = require('./model')
 
-function connectMongo(excute) {
-    // mongodb://admin:785689@cluster0-shard-00-00-koeuy.mongodb.net:27017,cluster0-shard-00-01-koeuy.mongodb.net:27017,cluster0-shard-00-02-koeuy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin
-    // mongodb://admin:785689@lescluster-shard-00-00-njhnj.mongodb.net:27017,lescluster-shard-00-01-njhnj.mongodb.net:27017,lescluster-shard-00-02-njhnj.mongodb.net:27017/test?ssl=true&replicaSet=lesCluster-shard-0&authSource=admin
-    mongoose.connect('mongodb://admin:785689@lescluster-shard-00-00-njhnj.mongodb.net:27017,lescluster-shard-00-01-njhnj.mongodb.net:27017,lescluster-shard-00-02-njhnj.mongodb.net:27017/test?ssl=true&replicaSet=lesCluster-shard-0&authSource=admin');
+function connectMongo (excute) {
+  // mongodb://admin:785689@cluster0-shard-00-00-koeuy.mongodb.net:27017,cluster0-shard-00-01-koeuy.mongodb.net:27017,cluster0-shard-00-02-koeuy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin
+  // mongodb://admin:785689@lescluster-shard-00-00-njhnj.mongodb.net:27017,lescluster-shard-00-01-njhnj.mongodb.net:27017,lescluster-shard-00-02-njhnj.mongodb.net:27017/test?ssl=true&replicaSet=lesCluster-shard-0&authSource=admin
+  mongoose.connect('mongodb://admin:785689@lescluster-shard-00-00-njhnj.mongodb.net:27017,lescluster-shard-00-01-njhnj.mongodb.net:27017,lescluster-shard-00-02-njhnj.mongodb.net:27017/test?ssl=true&replicaSet=lesCluster-shard-0&authSource=admin')
 
-    const db = mongoose.connection;
+  const db = mongoose.connection
 
-    db.on('error', (msg) => {
-        console.log('Error mesage')
-        console.log(msg)
-    })
+  db.on('error', (msg) => {
+    console.log('Error mesage')
+    console.log(msg)
+  })
 
-    db.once('open', () => {
-        console.log('Connected to MongoDB')
-        excute(db)
-    })
+  db.once('open', () => {
+    console.log('Connected to MongoDB')
+    excute(db)
+  })
 }
-
+const unmatched = []
 const database = {}
+
+let left = 28
 Object.keys(info).forEach((el, index) => {
-    const name = info[index].number.split(/[^0-9a-zA-Z]/).join('').toLowerCase()
-    const magnet = []
-    // database['number'] = info[index].number
-    // database['pic'] = info[index].pic
-    let matched = 'disappear'
-    for (let i in datamag) {
-        const title = datamag[i].title[0].split(/[^0-9a-zA-Z]/).join('').toLowerCase()
-        if (title.indexOf(name) > -1) {
-            matched = 'match'
-            magnet.push({
-                name: datamag[i].title[0],
-                size: datamag[i].size[0],
-                href: datamag[i].magnet[0]
-            })
-        }
+  const name = info[index].number.split(/[^0-9a-zA-Z]/).join('').toLowerCase()
+  const magnet = []
+  const avatar = info[index].avatar
+  for (let i in datamag) {
+    const title = datamag[i].title[0].split(/[^0-9a-zA-Z]/).join('').toLowerCase()
+    let count = 0
+    // if (title.indexOf(name) > -1) {
+    //     magnet.push({
+    //         name: datamag[i].title[0],
+    //         size: datamag[i].size[0],
+    //         href: datamag[i].magnet[0]
+    //     })
+    // }
+    avatar.forEach(ele => {
+      if (title.indexOf(ele.toLowerCase().replace(/\s/g, '')) > -1) {
+        count++
+      }
+    })
+    if (count > 1) {
+      // console.log(avatar, info[index].number, datamag[i].title[0])
+      magnet.push({
+        name: datamag[i].title[0],
+        size: datamag[i].size[0],
+        href: datamag[i].magnet[0]
+      })
     }
-    if (magnet.length) {
-        database[name] = {
-            number: info[index].number,
-            pic: info[index].pic,
-            info: `<p>片名: ${info[index].number}</p>`,
-            fake: false,
-            insertDate: Number(Date.now()),
-            magnet,
-        }
-    }
+  }
+  if (magnet.length) {
+  //     // database[name] = {
+  //     //     number: info[index].number,
+  //     //     pic: info[index].pic,
+  //     //     info: `<p>片名: ${info[index].number}</p>`,
+  //     //     fake: false,
+  //     //     insertDate: Number(Date.now()),
+  //     //     magnet,
+  //     // }
+  } else {
+      unmatched.push(info[index])
+  }
 
 })
+console.log(unmatched)
+function save2Atlas (bson) {
+  return new Promise((resolve, reject) => {
 
-function save2Atlas(bson) {
-    return new Promise((resolve, reject) => {
-
-        const item = new Store(bson)
-        item.save(err => {
-            if (!err) {
-                resolve('saved')
-            } else {
-                reject('failed')
-            }
-
-        })
+    const item = new Store(bson)
+    item.save(err => {
+      if (!err) {
+        resolve('saved')
+      } else {
+        reject('failed')
+      }
     })
+  })
 }
-// console.log(Object.keys(database))
-// Object.keys(database).forEach((ele, index) => {
-//     if (!index) {
-//         console.log(database)
-//         save2Atlas(database[ele]).then(msg => console.log(msg))
-//     }
-//     // save2Atlas(database[ele]).then(msg => console.log(msg))
+
+// connectMongo(db => {
+//     Object.keys(database).forEach((ele, index) => {
+//         if (index) {
+//             save2Atlas(database[ele]).then(msg => { console.log(msg); db.close() })
+//         }
+//     })
 // })
 
-connectMongo(db => {
-    Object.keys(database).forEach((ele, index) => {
-        if (index) {
-            // console.log(database)
-            save2Atlas(database[ele]).then(msg => { console.log(msg); db.close() })
-        }
-        // save2Atlas(database[ele]).then(msg => console.log(msg))
-    })
-})
+// console.log(unmatched)
+// const tmpJSON = {}
+// unmatched.forEach((el, index) => {
+//     tmpJSON[index] = el
+// })
+//  jsonfile.writeFileSync('../json/wyfiltered.json', tmpJSON)
--- network.js
+++ network.js
@@ -16,47 +16,65 @@ let count = 1
 let indexDB = []
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 puppeteer.launch().then(async browser => {
-    let count = 1
-    let indexDB = []
-    while (count < 7) {
-        const page = await browser.newPage();
-        const url = 'https://www.webyoung.com/en/videos/updates/All-Categories/0/All-Pornstars/0/' + count
-        const [networkErr] = await to(page.goto(url, {
-            waitUntil: 'domcontentloaded'
-        }))
-        if (networkErr) {
-            await page.close()
-            console.log('continue')
-            continue
-        }
-        const result = await page.evaluate(() => {
-            const items = document.querySelectorAll('.tlcItem')
-            return [].map.call(items, item => {
-                const pic = item.querySelector('img').getAttribute('data-original')
-                const number = item.querySelector('.tlcTitle>a').getAttribute('title')
-                return {
-                    number,
-                    pic
-                }
-            })
-        })
-        await page.close()
-        indexDB = indexDB.concat(result)
-        console.log(indexDB.length)
-        count++
-    }
-    await browser.close()
-    console.log(indexDB.length)
-    const indexJSON = {}
-    indexDB.forEach((ele, index) => {
-        indexJSON[index] = ele
-    })
-    
-    jsonfile.writeFileSync('./json/wyinfo.json', indexJSON)
+    // let count = 1
+    // let indexDB = []
+    // while (count < 7) {
+    //     const page = await browser.newPage();
+    //     const url = 'https://www.webyoung.com/en/videos/updates/All-Categories/0/All-Pornstars/0/' + count
+    //     const [networkErr] = await to(page.goto(url, {
+    //         waitUntil: 'domcontentloaded'
+    //     }))
+    //     if (networkErr) {
+    //         await page.close()
+    //         console.log('continue')
+    //         continue
+    //     }
+    //     const result = await page.evaluate(() => {
+    //         const items = document.querySelectorAll('.tlcItem')
+    //         return [].map.call(items, item => {
+    //             const pic = item.querySelector('img').getAttribute('data-original')
+    //             const number = item.querySelector('.tlcTitle>a').getAttribute('title')
+    //             const avatar = [].map.call(item.querySelectorAll('.tlcActors>a'), a => a.innerHTML)
+    //             return {
+    //                 number,
+    //                 pic,
+    //                 avatar
+    //             }
+    //         })
+    //     })
+    //     await page.close()
+    //     indexDB = indexDB.concat(result)
+    //     console.log(indexDB.length)
+    //     count++
+    // }
+    // await browser.close()
+    // console.log(indexDB.length)
+    // const indexJSON = {}
+    // indexDB.forEach((ele, index) => {
+    //     indexJSON[index] = ele
+    // })
+
+    const page = await browser.newPage();
+    // await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:57.0) Gecko/20100101 Firefox/56.0')
+    const [networkErr] = await to(page.goto('http://digitalplayground.com/tags/', {
+        waitUntil: 'networkidle0'
+    }))
+    // jsonfile.writeFileSync('./json/wyinfo.json', indexJSON)
     // await page.evaluate(() => {
     //     window.scrollBy(0, window.innerHeight);
     // })
-    // await timeout(5000)
-    // const html = await page.content()
-    // fs.writeFileSync('index.html', html)
+    await timeout(5000)
+    // if (networkErr)
+    const [error, html] = await to(page.content())
+    // const [error, html] = await to(page.evaluate(() => {
+    //     const wrapper = document.querySelector('html')
+    //     return wrapper.outerHTML
+    // }))
+    if (error) {
+        console.log(error)
+    } else {
+        fs.writeFileSync('index.html', html)
+    }
+
+    await browser.close()
 })
